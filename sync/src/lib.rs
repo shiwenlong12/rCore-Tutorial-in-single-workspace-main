70c369b37c7f44449fc9a6a12a1e2b943987e9e6
shiwenlong12 <1300160147@qq.com>
2023-03-15 22:23:03 +0800

    3.15

---
+++ sync/src/lib.rs
@@ -0,0 +1,26 @@
+//! 同步互斥模块
+
+#![no_std]
+#![deny(warnings, missing_docs)]
+
+mod condvar;
+mod mutex;
+mod semaphore;
+mod up;
+
+extern crate alloc;
+
+pub use condvar::Condvar;
+pub use mutex::{Mutex, MutexBlocking};
+pub use semaphore::Semaphore;
+pub use up::{UPIntrFreeCell, UPIntrRefMut};
+
+
+# [cfg(test)]
+mod tests{
+
+    #[test]
+    fn test_() {
+        
+    }
+}
